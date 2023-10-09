093d094acab440bd2d930dd95a8777fae7b7e2fa
CodeProSpecialist <111866070+CodeProSpecialist@users.noreply.github.com>
2023-10-09 14:01:10 -0500

    Create speedtest.cpp

---
+++ speedtest.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <chrono>
+
+int main() {
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (int i = 1; i <= 100000; ++i) {
+        std::cout << i << std::endl;
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
+
+    std::cout << "Total time taken: " << duration.count() << " seconds" << std::endl;
+
+    return 0;
+}
